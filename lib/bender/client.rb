68172fff75e0e4c7c9f4c49d8e819c17633ab007
Anthony Leak <anthony.leak@statpro.com>
2015-01-28 09:21:57 +0200

    changed aws env key

--- lib/bender/client.rb
+++ lib/bender/client.rb
@@ -23,7 +23,7 @@ module Bender
       def sqs
         @@sqs ||= AWS::SQS.new({
           :access_key_id => ENV['AWS_ACCESS_KEY'],
-          :secret_access_key => ENV['AWS_SECRET_ACCESS'],
+          :secret_access_key => ENV['AWS_SECRET_KEY'],
           :region => ENV['AWS_REGION']
         })
       end
